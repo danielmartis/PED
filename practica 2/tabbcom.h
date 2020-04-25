16012ae486d75d1f80ffee7237f83e2e4ffc682c
daniel <47526269+shinatsuhiko@users.noreply.github.com>
2020-04-25 02:20:53 +0200

    tabbcom completado (falta testear)

--- practica 2/tabbcom.h
+++ practica 2/tabbcom.h
@@ -8,8 +8,75 @@
 #include "tvectorcom.h"
 
 using namespace std;
-
+class TNodoABB;
+class TABBCom {
+    friend class TNodoABB;
+private:
+    //Puntero al nodo
+    TNodoABB * nodo;
+    
+    //AUXILIARES de los recorridos.
+    void InordenAux(TVectorCom &, int &) const;
+    void PreordenAux(TVectorCom &, int &) const;
+    void PostordenAux(TVectorCom &, int &) const;
+public:
+    //Constructor por defecto
+    TABBCom();
+    
+    //Cons de copia
+    TABBCom(TABBCom &);
+    
+    // Destructor
+    ~TABBCom ();
+    
+    // Sobrecarga del operador asignación
+    TABBCom & operator=(const TABBCom &);
+    
+    //Sobrecarga del operador igualdad
+    bool operator==(const TABBCom &) const;
+    
+    //Devuelve TRUE si el árbol esta vacío, FALSE  en caso contrario
+    bool EsVacio() const;
+    
+    // Inserta el elemento en el árbol
+    bool Insertar(const TComplejo &);
+    
+    // Borra el elemento en el árbol
+    bool Borrar(TComplejo &);
+    
+    // Devuelve TRUE si el elemento está en el árbol, FALSE en caso contrario
+    bool Buscar(const TComplejo &) const;
+    
+    // Devuelve el elemento en la raíz del árbol
+    TComplejo Raiz();
+    
+    //Devuelve la altura del árbol (la altura de un árbol vacío es 0)
+    int Altura();
+    
+    //Devuelve el número de nodos del árbol (un árbol vacío tiene 0 nodos)
+    int Nodos() const;
+    
+    // Devuelve el número de nodos hoja en el árbol (la raíz puede ser nodo hoja)
+    int NodosHoja();
+    //Devuelve el recorrido en inorden
+    TVectorCom Inorden() const;
+    
+    //Devuelve el recorrido en Preorden
+    TVectorCom Preorden() const;
+    
+    //Devuelve el recorrido en Postorden.
+    TVectorCom Postorden() const;
+    
+    //Devuelve el recorrido por niveles
+    TVectorCom Niveles() ;
+    
+    //Sobrecarga del operador salida
+    friend ostream & operator<<(ostream&,TABBCom &);
+    
+    TComplejo ConseguirMayor() const;
+};
 class TNodoABB {
+    friend class TABBCom;
 private:
     //Elemento del nodo
     TComplejo item;
@@ -26,70 +93,7 @@ public:
     TNodoABB & operator=(TNodoABB &);
 };
 
-class TABBCom {
-private:
-    //Puntero al nodo
-    TNodoABB * nodo;
-    
-    //AUXILIARES de los recorridos.
-    void InordenAux(TVectorCom &, int &);
-    void PreordenAux(TVectorCom &, int &);
-    void PostordenAux(TVectorCom &, int &);
-public:
-    //Constructor por defecto
-    TABBCom();
-    
-    //Cons de copia
-    TABBCom(TABBCom &);
-    
-    // Destructor
-    ~TABBCom ();
-    
-    // Sobrecarga del operador asignación
-    TABBCom & operator=(TABBCom &);
-    
-    //Sobrecarga del operador igualdad
-    bool operator==(TABBCom &);
-    
-    //Devuelve TRUE si el árbol esta vacío, FALSE  en caso contrario
-    bool EsVacio();
-    
-    // Inserta el elemento en el árbol
-    bool Insertar(TComplejo &);
-    
-    // Borra el elemento en el árbol
-    bool Borrar(TComplejo &);
-    
-    // Devuelve TRUE si el elemento está en el árbol, FALSE en caso contrario
-    bool Buscar(TComplejo &);
-    
-    // Devuelve el elemento en la raíz del árbol
-    TComplejo Raiz();
-    
-    //Devuelve la altura del árbol (la altura de un árbol vacío es 0)
-    int Altura();
-    
-    //Devuelve el número de nodos del árbol (un árbol vacío tiene 0 nodos)
-    int Nodos();
-    
-    // Devuelve el número de nodos hoja en el árbol (la raíz puede ser nodo hoja)
-    int NodosHoja();
-    //Devuelve el recorrido en inorden
-    TVectorCom Inorden();
-    
-    //Devuelve el recorrido en Preorden
-    TVectorCom Preorden();
-    
-    //Devuelve el recorrido en Postorden.
-    TVectorCom Postorden();
-    
-    //Devuelve el recorrido por niveles
-    TVectorCom Niveles();
-    
-    //Sobrecarga del operador salida
-    friend ostream & operator<<(ostream&, TABBCom &);
-    
-};
+
 
 
 
